fbeed91a22ca17aa683b65578d7001086a961184
Sanket Savani <sanketsavani004@gmail.com>
2024-12-26 08:51:31 +0530

    climbing stairs

---
+++ daily_leetcode/climbing_stairs.cpp
@@ -0,0 +1,18 @@
+// 70. Climbing Stairs [EASY]
+// Date: Dec 26, 2024
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int one = 1;
+        int two = 1;
+        int temp;
+
+        for (int i = 0; i < n - 1; i++){
+            temp = one;
+            one = one + two;
+            two = temp;
+        }
+        return one;
+    }
+};
